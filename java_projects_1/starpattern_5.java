3729faad02713d8ac922742c32a1b940c0491287
Kerston2104 <113289902+Kerston2104@users.noreply.github.com>
2023-07-18 23:56:11 +0530

    STAR PATTERN UPDATE

--- java_projects_1/starpattern_5.java
+++ java_projects_1/starpattern_5.java
@@ -5,20 +5,18 @@
 //  ***
 // **** 
 
-package java_projects_1;// This line denotes the folder of the file where it is stored
+package java_projects_1;
 
-public class starpattern_5 {//class name should be created sames as file name
+public class starpattern_5 {
     public static void main(String[] args) {
-        int rows = 4; 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows - i; j++) {
-                System.out.print(" ");
+        int row =5;
+        for(int i=0; i<row; i++)
+            {
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
             }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+                System.out.println();
+            }    
     }
 }
 
