3a99fdac21e0fc771fb02b1e370b1191d6df06ee
Kerston2104 <113289902+Kerston2104@users.noreply.github.com>
2023-07-19 23:06:49 +0530

    added procedure

--- java_projects_2/FirstNonRepeatedCharacter_7.java
+++ java_projects_2/FirstNonRepeatedCharacter_7.java
@@ -1,6 +1,12 @@
 // Write a program in Java to find the first non-repeated character in a character array. For
 // example, the first non-repeated character in the input 'abcdab' is 'c'.
 
+// 1.The program prompts the user for a character array input.
+// 2.It counts the occurrences of each character using an integer array count.
+// 3.The first non-repeated character is found by looping through the array.
+// 4.If found, it is returned as the result; otherwise, '\0' is returned.
+// 5.The result is displayed to the user.
+
 package java_projects_2;//this line is only needed if you are using your codes under any folder named java_projects_2
 
 import java.util.Scanner;
