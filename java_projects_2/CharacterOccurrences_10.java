11fe9d25f10510fea9e09185980ea0b714ff94a1
Kerston2104 <113289902+Kerston2104@users.noreply.github.com>
2023-07-19 21:59:29 +0530

    ADDED_NEWFILE

--- java_projects_2/CharacterOccurrences_10.java
+++ java_projects_2/CharacterOccurrences_10.java
@@ -34,4 +34,3 @@ public class CharacterOccurrences_10 {
         return count;
     }
 }
-
---
+++ java_projects_2/HighestOccurredCharacter_10_2.java
@@ -0,0 +1,38 @@
+package java_projects_2;
+
+
+import java.util.Scanner;
+
+public class HighestOccurredCharacter_10_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the character array (e.g., abcdaabcab): ");
+        String input = sc.nextLine();
+        char[] inputArray = input.toCharArray();//changing string to character.
+
+        char highestOccurredChar = findHighestOccurredCharacter(inputArray);
+        System.out.println("The highest occurred character is: " + highestOccurredChar);
+    }
+
+    public static char findHighestOccurredCharacter(char[] arr) {
+        int[] charOccurrences = new int[128]; // Assuming ASCII characters
+        for (char ch : arr) {
+            charOccurrences[ch]++;
+        }
+
+        char highestChar = '\0';
+        int maxOccurrences = 0;
+        for (char ch : arr) {
+            if (charOccurrences[ch] > maxOccurrences) {
+                highestChar = ch;
+                maxOccurrences = charOccurrences[ch];
+            }
+        }
+
+        return highestChar;
+    }
+}
+
+
+
